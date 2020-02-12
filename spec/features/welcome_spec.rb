d471d144380dbd278674d42f976311f7350926a6
Andrew Oliveira <andrewhenriquef@gmail.com>
2020-02-11 21:55:35 -0300

    (+) Create a feature spec for welcome page

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.feature "Welcomes", type: :feature do
+  scenario 'Show welcome message' do
+    visit('/')
+
+    expect(page).to have_content('Welcome')
+  end
+end
