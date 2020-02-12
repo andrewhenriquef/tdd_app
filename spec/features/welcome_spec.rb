4b432b88251967242986fc32c9e3a714736f8f38
Andrew Oliveira <andrewhenriquef@gmail.com>
2020-02-11 22:18:34 -0300

    (+) Add a spec feature to check if a button exists on page

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "Welcomes", type: :feature do
   scenario 'Show welcome message' do
-    visit('/')
+    visit root_path
 
     expect(page).to have_content('Welcome')
+  end
+
+  scenario 'Validate if clients registration link exists on page' do
+    visit root_path
+    expect(find('ul li')).to have_link('Clients registration')
   end
 end
