5ea28f0af3703221f7fd2c289cd58b6f328f6164
Andrew Oliveira <andrewhenriquef@gmail.com>
2020-02-18 21:26:41 -0300

    (+) Create form to register a new client

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,28 @@
 class CustomersController < ApplicationController
   def index; end
   def show; end
-  def new; end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to @customer, notice: 'Client succesfully registered'
+    else
+      render :new
+    end
+  end
+
+  def customer_params
+    params.require(:customer).permit(
+      :id,
+      :name,
+      :email,
+      :phone,
+      :avatar,
+      :smoker
+    )
+  end
 end
